48fc1662741aff976f381c8901a0e495dda5dd8f
Guield <guillemendez02@gmail.com>
2024-02-17 19:04:02 -0600

    segundo commit

---
+++ src/app/components/data.js
@@ -0,0 +1,70 @@
+export const data=[
+    {
+    id:1,
+    title:'Cien años de soledad',
+    price:100,
+    urlImage:'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity:1
+    },
+    {
+    id:2,
+    title:'El señor de los anillos(Trilogía)',
+    price:190,
+    urlImage:'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    quantity:1
+    },
+    {
+    id:3,
+    title:'Cuentos de Barro',
+    price:30,
+    urlImage:'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+    quantity:1
+    },
+    {
+    id:4,
+    title:'Tierra de Infancia',
+    price:30,
+    urlImage:'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+    quantity:1
+    }, {
+    id:5,
+    title:'Harry Potter Pack',
+    price:390,
+    urlImage:'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+    quantity:1
+    },
+    {
+        id:6,
+        title:'IT',
+        price:300,
+        urlImage: 'https://imagessl3.casadellibro.com/a/l/s7/93/9788497593793.webp',
+        quantity:1
+        }, {
+            id:7,
+            title:'La paciente silenciosa',
+            price:300,
+            urlImage: 'https://imagessl5.casadellibro.com/a/l/s7/35/9788466351935.webp',
+            quantity:1
+            },
+            {
+                id:8,
+                title:'El primer caso de unamuno',
+                price:300,
+                urlImage: 'https://imagessl6.casadellibro.com/a/l/s7/66/9788420476766.webp',
+                quantity:1
+                },
+                {
+                    id:9,
+                    title:'Tan poca vida',
+                    price:300,
+                    urlImage: 'https://imagessl8.casadellibro.com/a/l/s7/78/9788426403278.webp',
+                    quantity:1
+                    },
+                    {
+                        id: 10,
+                        title:'La sociedad de la nieve',
+                        price:300,
+                        urlImage: 'https://imagessl2.casadellibro.com/a/l/s7/32/9788418584732.webp',
+                        quantity:1
+                        }
+    ];
